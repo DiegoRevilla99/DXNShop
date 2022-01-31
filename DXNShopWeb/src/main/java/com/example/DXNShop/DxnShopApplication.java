c12dd7af2ffe081f86a6b969801e26470d8dc1bd
DiegoRevilla99 <75765142+DiegoRevilla99@users.noreply.github.com>
2022-01-31 02:27:42 -0600

    Archivo inicial

---
+++ DXNShopWeb/src/main/java/com/example/DXNShop/DxnShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.DXNShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxnShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxnShopApplication.class, args);
+	}
+
+}
